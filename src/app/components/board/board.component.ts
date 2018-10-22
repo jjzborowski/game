1e20528a2982222867ee13d9aff5a69192fd40a7
Jacek Zborowski <j.j.zborowski@gmail.com>
2018-10-22 10:05:22 +0200

    Board styling

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -10,20 +10,92 @@ import { BoardService } from '@services';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
-  private gridData = {
-    x: 0,
-    y: 0,
-    tile: {}
+  public boardData = {
+    0: {
+      0: {
+        define: true,
+        name: 'test 0'
+      }
+    }
   };
+  private maxColumnIndex = 1;
+  private maxRowIndex = 1;
+  private minColumnIndex = -1;
+  private minRowIndex = -1;
+  private tileIndex = 0;
   private viewPositionX = 0;
   private viewPositionY = 0;
   private zoom = 0;
 
   constructor(private boardService: BoardService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.fillBoardData();
+  }
 
-  private addTile() { }
+  private fillBoardData(): void {
+    for (let rowIndex = this.minRowIndex; rowIndex < this.maxRowIndex + 1; rowIndex++) {
+      if (!this.boardData[rowIndex]) {
+        this.boardData[rowIndex] = {};
+      }
+      for (let columnIndex = this.minColumnIndex; columnIndex < this.maxColumnIndex + 1; columnIndex++) {
+        if (!this.boardData[rowIndex][columnIndex]) {
+          this.boardData[rowIndex][columnIndex] = {
+            define: false
+          };
+        }
+      }
+    }
+    console.log(this.boardData);
+  }
+
+  private defineNextTiles(rowIndex: number, columnIndex: number): void {
+    if (this.boardData[rowIndex - 1][columnIndex - 1]) {
+
+    }
+    if (this.boardData[rowIndex - 1][columnIndex]) {
+
+    }
+    if (this.boardData[rowIndex - 1][columnIndex + 1]) {
+
+    }
+    if (this.boardData[rowIndex][columnIndex - 1]) {
+
+    }
+    if (this.boardData[rowIndex][columnIndex + 1]) {
+
+    }
+    if (this.boardData[rowIndex + 1][columnIndex - 1]) {
+
+    }
+    if (this.boardData[rowIndex + 1][columnIndex]) {
+
+    }
+    if (this.boardData[rowIndex + 1][columnIndex + 1]) {
+
+    }
+  }
+
+  private addTile(rowIndex: number, columnIndex: number): void {
+    if (!this.boardData[rowIndex][columnIndex].define) {
+      this.tileIndex++;
+      this.boardData[rowIndex][columnIndex] = {
+        define: true,
+        name: `test ${this.tileIndex}`
+      };
+      if (rowIndex === this.minRowIndex) {
+        this.minRowIndex--;
+      } else if (rowIndex === this.maxRowIndex) {
+        this.maxRowIndex++;
+      }
+      if (columnIndex === this.minColumnIndex) {
+        this.minColumnIndex--;
+      } else if (columnIndex === this.maxColumnIndex) {
+        this.maxColumnIndex++;
+      }
+      this.fillBoardData();
+    }
+  }
 
   private moveViewVertical(): void { }
 
